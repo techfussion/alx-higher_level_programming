fe5afad4996d479c6779661d49a1dd47e583900d
techfussion <muminraj15@gmail.com>
2023-06-12 22:33:52 -0700

    script 8-multiple_returns

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,12 +5,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
     for i in range(2):
         try:
             item_1 = tuple_a[i]
-        except:
+        except IndexError:
             item_1 = 0
 
         try:
             item_2 = tuple_b[i]
-        except:
+        except IndexError:
             item_2 = 0
 
         result.append(item_1 + item_2)
