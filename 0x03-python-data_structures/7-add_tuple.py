df3cac6d10aed785ababae2bef2681e7258f054a
techfussion <muminraj15@gmail.com>
2023-06-12 22:11:53 -0700

    script 7-add_tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    result = []
+
+    for i in range(2):
+        try:
+            item_1 = tuple_a[i]
+        except:
+            item_1 = 0
+
+        try:
+            item_2 = tuple_b[i]
+        except:
+            item_2 = 0
+
+        result.append(item_1 + item_2)
+
+    return tuple(result)
