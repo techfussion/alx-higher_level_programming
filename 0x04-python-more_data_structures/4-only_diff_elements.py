84ebffbe9f16d03496bf954c3b74be829230964e
techfussion <muminraj15@gmail.com>
2023-06-15 12:01:33 -0700

    script 4-only_diff_elements

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    od_set = set()
-
-    for item in set_1:
-        od_set.add(item)
-
-    for item in set_2:
-        od_set.add(item)
-
-    return od_set
+    return set_1.union(set_2)
