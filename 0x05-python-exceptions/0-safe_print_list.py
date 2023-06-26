ac400af9656ef8251cbbe990e3eed0b02139c84f
techfussion <muminraj15@gmail.com>
2023-06-26 06:41:43 -0700

    script 0-safe_print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,7 @@ def safe_print_list(my_list=[], x=0):
                 return nb_print
 
             print(item, end='')
+            nb_print += 1
         print()
     except IndexError:
         pass
