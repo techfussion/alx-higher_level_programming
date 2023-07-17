fc2c6087bd432b1a19f0836c3025b9598db9df97
techfussion <muminraj15@gmail.com>
2023-07-16 18:42:01 -0700

    script 1-rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,10 +6,15 @@ class Rectangle:
     """Class representation"""
 
     def __init__(self, width=0, height=0):
-        """Global instantiation attributes"""
+        """Global instantiation attributes.
 
-        self.__width = width
-        self.__height = height
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
